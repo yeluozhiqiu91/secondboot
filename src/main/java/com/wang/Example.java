91768ba2f0ef20300fdb11060f4819ef72b9446f
wangliujie <wangliujie@phone580.com>
2018-11-20 19:48:15 +0800

    最简单的例子

---
+++ src/main/java/com/wang/Example.java
@@ -0,0 +1,22 @@
+package com.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: wangliujie
+ * @Date: 2018/11/20 19:42
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+}
